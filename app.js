1ae3849a9115e60aaf63118404e3a297141bb59b
Nemoralia <f3nriblack@gmail.com>
2025-03-02 16:40:48 -0500

    "se crear un metodo get para cerrar sesión"

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const cors = require('cors')
+const cors = require('cors');
 const mysql = require('mysql2/promise');
 const session = require('express-session');
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
-app.use(session({
-    secret: "12345"
-}))
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
+app.use(session({
+  secret: "12345",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Asegúrate de que secure esté en false para desarrollo
+}));
 
 // Create the connection to database
 const connection = mysql.createPool({
@@ -25,36 +28,39 @@ app.get("/", (req, res) => {
   res.send("Inicio");
 });
 
-
-app.get("/login",async (req, res) => {
-    const datos = req.query;
-    // A simple SELECT query
-try {
+app.get("/login", async (req, res) => {
+  const datos = req.query;
+  try {
     const [results, fields] = await connection.query(
       "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
       [datos.usuario, datos.clave]
     );
     if (results.length > 0) {
-        req.session.usuario = datos.usuario;
-        res.status(200).send("Inicio sesión correcto");
+      req.session.usuario = datos.usuario;
+      res.status(200).send("Inicio sesión correcto");
     } else {
-        res.status(401).send("Inicio sesión incorrecto");
+      res.status(401).send("Inicio sesión incorrecto");
     }
-    
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
   } catch (err) {
     console.log(err);
   }
 });
 
-
 app.get("/validar", (req, res) => {
-    if(req.session.usuario){
-        res.status(200).json({ usuario: req.session.usuario });
-    }else{
-        res.status(401).send("Sesion no validada");
+  if (req.session.usuario) {
+    res.status(200).json({ usuario: req.session.usuario });
+  } else {
+    res.status(401).send("Sesion no validada");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Error al cerrar sesión");
     }
+    res.status(200).send("Sesión cerrada correctamente");
+  });
 });
 
 app.listen(port, () => {
